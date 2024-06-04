435bb7351c8f93dce8782ae56fae77de440170db
MayconEidam <127507968+MayconEidam@users.noreply.github.com>
2024-06-04 11:49:09 -0300

    ok

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const caixaPrincipal = document.querySelectorAll(".caixa-principal");
+const caixaPergunta = document.querySelectorAll(".caixa-pergunta");
+const caixaAlternativa = document.querySelectorAll(".caixa-alternativa");
+const caixaPrincipal = document.querySelectorAll(".caixa-principal");
+const caixaPrincipal = document.querySelectorAll(".caixa-principal");
