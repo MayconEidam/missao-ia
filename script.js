467a3724dcfe077164f925e28ece2bd6359dac12
MayconEidam <127507968+MayconEidam@users.noreply.github.com>
2024-06-04 11:57:51 -0300

    ok

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const caixaPergunta = document.querySelectorAll(".caixa-pergunta");
 const caixaAlternativa = document.querySelectorAll(".caixa-alternativa");
 const caixaPrincipal = document.querySelectorAll(".caixa-principal");
 const caixaPrincipal = document.querySelectorAll(".caixa-principal");
+
+const perguntas = [
+{
+    enunciado: "pergunta1", alternativas:["alternativa1", "alternativa2"]
+},
+{
+    enunciado: "pergunta1", alternativas:["alternativa1", "alternativa2"]
+}
+];
