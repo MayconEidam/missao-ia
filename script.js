b96dfdb26daa5a60241d63e7f11ac169a216daf1
MayconEidam <127507968+MayconEidam@users.noreply.github.com>
2024-06-11 11:46:57 -0300

    ok

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ const caixaPrincipal = document.querySelectorAll(".caixa-principal");
 
 const perguntas = [
 {
-    enunciado: "pergunta1", 
-    alternativas:["alternativa1", "alternativa2"]
+    enunciado: "Qual a melhor maneira de combater o desmatamento?", 
+    alternativas:[
+        "Reflorestamento de área degradadas", 
+        "Implementação de leis mais rigorosas para a proteção das florestas"]
 },
 {
-    enunciado: "pergunta1", 
-    alternativas:["alternativa1", "alternativa2"]
+    enunciado: "Como podemos combater a desigualdade social de forma eficaz e sustentável?", 
+    alternativas:[
+        "Implementar programas de educação e formação profissional acessíveis a todos, capacitando indivíduos de comunidades desfavorecidas.", 
+        "políticas de redistribuição de renda e oportunidades,ppromovendo uma distribuição igual dos recursos e do acesso aos benefícios"]
 }
 ];
