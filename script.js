bf098ca74ef21072af023876fe09d81864f0c242
MayconEidam <127507968+MayconEidam@users.noreply.github.com>
2024-07-03 12:05:46 -0300

    ok

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-pergunta");
-const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
 {
     enunciado: "Qual a melhor maneira de combater o desmatamento?", 
-    alternativas:[
+    alternativa:[
         "Reflorestamento de área degradadas", 
         "Implementação de leis mais rigorosas para a proteção das florestas"]
 },
 {
     enunciado: "Como podemos combater a desigualdade social de forma eficaz e sustentável?", 
-    alternativas:[
+    alternativa:[
         "Implementar programas de educação e formação profissional acessíveis a todos, capacitando indivíduos de comunidades desfavorecidas.", 
         "políticas de redistribuição de renda e oportunidades,ppromovendo uma distribuição igual dos recursos e do acesso aos benefícios"]
 }
@@ -24,6 +24,6 @@ let perguntaAtual;
 
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaPergunta.textContent = perguntaAtual.enunciado;
 }
 mostraPergunta();
